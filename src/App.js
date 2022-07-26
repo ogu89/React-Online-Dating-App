ae3d92a0f988df9b118c583132b5eff9293aee6c
aogu <aogu89goo415@gmail.com>
2022-07-26 14:41:15 +1000

    change to react HashRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home"
-import UserList from  "./pages/UserList"
-import UserDetail from "./pages/UserDetail"
-import Chat from "./pages/Chat"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import UserList from "./pages/UserList";
+import UserDetail from "./pages/UserDetail";
+import Chat from "./pages/Chat";
 import MessageList from "./pages/MessageList";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <Router> 
+    <HashRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/user' element={<UserList />} />
-        <Route path='/user/:id' element={<UserDetail />} />
-        <Route path='/user/:id/chat' element={<Chat />} />
-        <Route path='/message-list' element={<MessageList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<UserList />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+        <Route path="/user/:id/chat" element={<Chat />} />
+        <Route path="/message-list" element={<MessageList />} />
       </Routes>
-    </Router>
-    
+    </HashRouter>
   );
 }
 
